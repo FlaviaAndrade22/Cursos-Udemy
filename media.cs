808d23aec84464f3e04a7b2e2baf5034c093e53e
Flavia Andrade Ribeiro <143115629+FlaviaAndrade22@users.noreply.github.com>
2026-04-07 14:54:12 -0300

    media das materias

--- media.cs
+++ media.cs
@@ -1,23 +1,47 @@
-﻿namespace Ambiente_para_uso_dos_cursos
+﻿using System.ComponentModel.Design;
+
+namespace Ambiente_para_uso_dos_cursos
 {
     internal class media
     {
         static void Main(string[] args)
         {
-            double x, y, media;
+            double n1, n2, n3, media, nota;
+            string materia;
 
-            Console.WriteLine("Chose the firt number:");
-            x = double.Parse (Console.ReadLine());
+            Console.WriteLine("Insira o nome da matéria:");
+            materia = Console.ReadLine();
 
-            Console.WriteLine("Chose the segunt number:");
-            y = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Insira a media minima para passar na matéria de {materia}:");
+            media = double.Parse(Console.ReadLine());
 
-            media = (x + y) / 2;
+            Console.WriteLine("Primeira nota:");
+            n1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("media is :" + media );
+            Console.WriteLine("Segunda nota:");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("terceira nota:");
+            n3 = double.Parse(Console.ReadLine());
+
+            nota = (n1 + n2 + n3) / 3;
+
+
+
+            if (nota == media)
+            {
+                Console.WriteLine("Você esta na media");
+            }
+            else if (nota < media)
+            {
+                Console.WriteLine($"Você ficou abaixo da media. nota igual a {nota}");
+            }
+            else if (nota > media)
+            {
+                Console.WriteLine($"Você passou. nota igual a {nota}");
+            }
 
             return;
-
         }
     }
 }
