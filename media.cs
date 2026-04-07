9de0e8adc13cfe62dda60bd4cabc738f62f6ac2e
Flavia Andrade Ribeiro <143115629+FlaviaAndrade22@users.noreply.github.com>
2026-04-07 13:08:08 -0300

    Adicionar arquivos de projeto.

---
+++ media.cs
@@ -0,0 +1,23 @@
+﻿namespace Ambiente_para_uso_dos_cursos
+{
+    internal class media
+    {
+        static void Main(string[] args)
+        {
+            double x, y, media;
+
+            Console.WriteLine("Chose the firt number:");
+            x = double.Parse (Console.ReadLine());
+
+            Console.WriteLine("Chose the segunt number:");
+            y = double.Parse(Console.ReadLine());
+
+            media = (x + y) / 2;
+
+            Console.WriteLine("media is :" + media);
+
+            return;
+
+        }
+    }
+}
